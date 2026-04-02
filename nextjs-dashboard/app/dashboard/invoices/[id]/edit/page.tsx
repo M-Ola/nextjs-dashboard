3d7676ed1cf9a2fdde638c4b37e6733b45707de5
M-Ola <ola466@gmail.com>
2026-04-02 04:57:17 -0500

    corrected invoices

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -8,8 +8,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Edit Invoice',
 };
- 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+ export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
     const id = params.id;
     const [invoice, customers] = await Promise.all([
